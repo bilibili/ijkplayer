bcb1fbef8151557d39b179c8b54a06cb8a9808a3
Zhang Rui <bbcallen@gmail.com>
2013-06-05 14:25:31 +0800

    ijkmediaplayer: license files

--- ijkmediaplayer/jni/ffplay/pkt_queue.h
+++ ijkmediaplayer/jni/ffplay/pkt_queue.h
@@ -1,24 +1,24 @@
 /*****************************************************************************
- * pkt_queue.h
+ * pkt_queue.c
  *****************************************************************************
  *
  * copyright (c) 2001 Fabrice Bellard
  * copyright (c) 2013 Zhang Rui <bbcallen@gmail.com>
  *
- * Based on ffmpeg/ffplay.c
+ * This file is part of ijkPlayer.
  *
- * This program is free software; you can redistribute it and/or
+ * ijkPlayer is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * ijkPlayer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
+ * License along with ijkPlayer; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
--- ijkmediaplayer/src/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
+++ ijkmediaplayer/src/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
@@ -1,8 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
  * Copyright (C) 2013 Zhang Rui <bbcallen@gmail.com>
- * 
- * Based on android.media.MediaPlayer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
