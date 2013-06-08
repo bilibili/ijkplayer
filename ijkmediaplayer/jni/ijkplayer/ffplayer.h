eaf0c4924b5a548094fc14f5fa766fe49b68a5d7
Zhang Rui <bbcallen@gmail.com>
2013-06-08 11:38:49 +0800

    ijk: ffplayer: cosmetic

--- ijkmediaplayer/jni/ijkplayer/ffplayer.h
+++ ijkmediaplayer/jni/ijkplayer/ffplayer.h
@@ -39,23 +39,31 @@ typedef struct FFPlayer {
     AVDictionary   *codec_opts;
 
     /* ffplay options specified by the user */
-    // AVInputFormat  *file_iformat;
+#if 0
+    AVInputFormat  *file_iformat;
+#endif
     char           *input_filename;
-    // const char     *window_title;
-    // int             fs_screen_width;
-    // int             fs_screen_height;
-    // int             default_width  = 640;
-    // int             default_height = 480;
-    // int             screen_width  = 0;
-    // int             screen_height = 0;
+#if 0
+    const char     *window_title;
+    int             fs_screen_width;
+    int             fs_screen_height;
+    int             default_width  = 640;
+    int             default_height = 480;
+    int             screen_width  = 0;
+    int             screen_height = 0;
+#endif
     int             audio_disable;
     int             video_disable;
     int             subtitle_disable;
     int             wanted_stream[AVMEDIA_TYPE_NB];
     int             seek_by_bytes;
-    // int             display_disable;
+#if 0
+    int             display_disable;
+#endif
     int             show_status;
-    // int             av_sync_type = AV_SYNC_AUDIO_MASTER;
+#if 0
+    int             av_sync_type = AV_SYNC_AUDIO_MASTER;
+#endif
     int64_t         start_time;
     int64_t         duration;
     int             workaround_bugs;
@@ -67,37 +75,47 @@ typedef struct FFPlayer {
     enum AVDiscard  skip_idct;
     enum AVDiscard  skip_loop_filter;
     int             error_concealment;
-    // int             decoder_reorder_pts = -1;
+#if 0
+    int             decoder_reorder_pts = -1;
+#endif
     int             autoexit;
-    // int             exit_on_keydown;
-    // int             exit_on_mousedown;
+#if 0
+    int             exit_on_keydown;
+    int             exit_on_mousedown;
+#endif
     int             loop;
-    // int             framedrop = -1;
+#if 0
+    int             framedrop = -1;
+#endif
     int             infinite_buffer;
     enum ShowMode   show_mode;
-    char            *audio_codec_name;
-    char            *subtitle_codec_name;
-    char            *video_codec_name;
-    // double          rdftspeed = 0.02;
-    // int64_t         cursor_last_shown;
-    // int             cursor_hidden = 0;
-    #if CONFIG_AVFILTER
-    // char            *vfilters = NULL;
-    #endif
+    char           *audio_codec_name;
+    char           *subtitle_codec_name;
+    char           *video_codec_name;
+#if 0
+    double          rdftspeed = 0.02;
+    int64_t         cursor_last_shown;
+    int             cursor_hidden = 0;
+#endif
+#if CONFIG_AVFILTER
+    char           *vfilters;
+#endif
 
     /* current context */
-    //static int is_full_screen;
+#if 0
+    int             is_full_screen;
+#endif
     int64_t         audio_callback_time;
 
     /* callback */
-    int             (*decode_interrupt_cb)(void *ctx);
+    int           (*decode_interrupt_cb)(void *ctx);
 } FFPlayer;
 
 #define IJKFF_SAFE_FREE(p) do {free(p); p = NULL;} while(0)
 
 inline static void ijkff_reset(FFPlayer *ffp)
 {
-    /* FIXME: ffplay context reset */
+    /* FIXME: reset ffp->is */
 
     /* format/codec options */
     av_dict_free(&ffp->format_opts);
@@ -131,6 +149,9 @@ inline static void ijkff_reset(FFPlayer *ffp)
     IJKFF_SAFE_FREE(ffp->audio_codec_name);
     IJKFF_SAFE_FREE(ffp->subtitle_codec_name);
     IJKFF_SAFE_FREE(ffp->video_codec_name);
+#if CONFIG_AVFILTER
+    ffp->vfilters               = NULL;
+#endif
 
     /* current context */
     ffp->audio_callback_time    = 0;
