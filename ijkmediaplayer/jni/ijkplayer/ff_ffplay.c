d4bbe1697ee8c42e286cb2af11a4a4e99a3df04b
Zhang Rui <bbcallen@gmail.com>
2013-07-02 19:49:47 +0800

    FIXME: set video mode in msg_loop

--- ijkmediaplayer/jni/ijkplayer/ff_ffplay.c
+++ ijkmediaplayer/jni/ijkplayer/ff_ffplay.c
@@ -562,6 +562,7 @@ display:
 
 /* allocate a picture (needs to do that in main thread to avoid
    potential locking problems */
+// FIXME: 0 set video format in msg_loop
 static void alloc_picture(FFPlayer *ffp)
 {
     VideoState *is = ffp->is;
