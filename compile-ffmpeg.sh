b18fb194b3a9c5d39637904b7e8038f2f8c42aaf
Zhang Rui <bbcallen@gmail.com>
2013-07-02 17:40:27 +0800

    compile-ffmpeg.sh: build ffmpeg with c99

--- compile-ffmpeg.sh
+++ compile-ffmpeg.sh
@@ -113,6 +113,7 @@ export AR=${FF_CROSS_PREFIX}-ar
 export STRIP=${FF_CROSS_PREFIX}-strip
 
 FF_CFLAGS="-O3 -Wall -pipe \
+    -std=c99 \
     -ffast-math \
     -fstrict-aliasing -Werror=strict-aliasing \
     -fmodulo-sched -fmodulo-sched-allow-regmoves \
