5432283cf8017283650dea4161eaef00a28ec182
Zhang Rui <bbcallen@gmail.com>
2013-07-03 14:41:29 +0800

    compile-ffmpeg.sh: __ARM_ARCH_xx__ defined by gcc, not by us

--- compile-ffmpeg.sh
+++ compile-ffmpeg.sh
@@ -118,7 +118,6 @@ FF_CFLAGS="-O3 -Wall -pipe \
     -fstrict-aliasing -Werror=strict-aliasing \
     -fmodulo-sched -fmodulo-sched-allow-regmoves \
     -Wno-psabi -Wa,--noexecstack \
-    -D__ARM_ARCH_5__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5TE__ \
     -DANDROID -DNDEBUG"
 
 # --enable-thumb is OK
