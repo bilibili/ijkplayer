a76d05a473f048c3bd1b65e811cb55773689a7b8
Zhang Rui <bbcallen@gmail.com>
2013-08-09 19:08:55 +0800

    tools: update

--- tools/setup-as-commiter.sh
+++ tools/setup-as-commiter.sh
@@ -9,10 +9,10 @@ git branch -f master upstream/master
 git remote add mirrors --mirror=push git@github.com:bbcallen/FFmpeg.git
 git remote set-url mirrors --delete git@github.com:bbcallen/FFmpeg.git
 git remote set-url mirrors --add    git@github.com:bbcallen/FFmpeg.git
-git remote set-url mirrors --delete git@gitorious.org:bbcallen/FFmpeg.git
-git remote set-url mirrors --add    git@gitorious.org:bbcallen/FFmpeg.git
 git remote set-url mirrors --delete git@gitcafe.com:bbcallen/FFmpeg.git
 git remote set-url mirrors --add    git@gitcafe.com:bbcallen/FFmpeg.git
 git remote set-url mirrors --delete git@git.oschina.net:bbcallen/ffmpeg.git
 git remote set-url mirrors --add    git@git.oschina.net:bbcallen/ffmpeg.git
+git remote set-url mirrors --delete git@code.csdn.net:bbcallen/FFmpeg.git
+git remote set-url mirrors --add    git@code.csdn.net:bbcallen/FFmpeg.git
 
