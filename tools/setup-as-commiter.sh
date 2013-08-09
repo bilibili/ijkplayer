4681897037922d81a4709f6f0f69c996a8e621e3
bbcallen <bbcallen@gmail.com>
2013-08-10 01:33:43 +0800

    tools: setup ffmpeg and libav remote track

--- tools/setup-as-commiter.sh
+++ tools/setup-as-commiter.sh
@@ -2,17 +2,12 @@
 
 cd ffmpeg-armv7a
 
-git remote add upstream -f -t master git://source.ffmpeg.org/ffmpeg.git
-git fetch upstream
-git branch -f master upstream/master
+git remote set-url --push origin git@github.com:bbcallen/FFmpeg.git
 
-git remote add mirrors --mirror=push git@github.com:bbcallen/FFmpeg.git
-git remote set-url mirrors --delete git@github.com:bbcallen/FFmpeg.git
-git remote set-url mirrors --add    git@github.com:bbcallen/FFmpeg.git
-git remote set-url mirrors --delete git@gitcafe.com:bbcallen/FFmpeg.git
-git remote set-url mirrors --add    git@gitcafe.com:bbcallen/FFmpeg.git
-git remote set-url mirrors --delete git@git.oschina.net:bbcallen/ffmpeg.git
-git remote set-url mirrors --add    git@git.oschina.net:bbcallen/ffmpeg.git
-git remote set-url mirrors --delete git@code.csdn.net:bbcallen/FFmpeg.git
-git remote set-url mirrors --add    git@code.csdn.net:bbcallen/FFmpeg.git
+git remote add ffmpeg git://source.ffmpeg.org/ffmpeg.git
+git remote add libav git://git.libav.org/libav.git
+git fetch --all
+
+git branch --track ffmpeg ffmpeg/master
+git branch --track libav libav/master
 
