463dd9780e5f3e613bc8a5679024b0d8cf3b0612
Zhang Rui <bbcallen@gmail.com>
2013-08-16 15:43:21 +0800

    setup-as-commiter: setup mirros: gitcafe, oschina, csdn

--- android/setup-as-commiter.sh
+++ android/setup-as-commiter.sh
@@ -6,8 +6,11 @@ git remote set-url --push origin git@github.com:bbcallen/FFmpeg.git
 
 git remote add ffmpeg git://source.ffmpeg.org/ffmpeg.git
 git remote add libav git://git.libav.org/libav.git
+git remote add gitcafe git@gitcafe.com:bbcallen/FFmpeg.git
+git remote add oschina git@git.oschina.net:bbcallen/ffmpeg.git
+git remote add csdn git@code.csdn.net:bbcallen/FFmpeg.git
+
 git fetch --all
 
 git branch --track ffmpeg ffmpeg/master
 git branch --track libav libav/master
-
