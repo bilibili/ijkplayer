c09dba8e7bd7d2bdbf6a4a1c87e0068927e0026e
bbcallen <bbcallen@gmail.com>
2013-10-30 16:56:28 +0800

    ff_ffplay: merge ffplay 5c6a58746b415462f13c409aacb9041c98ab9ea0

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -1613,7 +1613,7 @@ static int audio_open(FFPlayer *opaque, int64_t wanted_channel_layout, int wante
     FFPlayer *ffp = opaque;
     SDL_AudioSpec wanted_spec, spec;
     const char *env;
-    const int next_nb_channels[] = {0, 0, 1, 6, 2, 6, 4, 6};
+    static const int next_nb_channels[] = {0, 0, 1, 6, 2, 6, 4, 6};
 
     env = SDL_getenv("SDL_AUDIO_CHANNELS");
     if (env) {
