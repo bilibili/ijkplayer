c155f6f51eb29b98aef45d60941fd2be323539cf
bbcallen <bbcallen@gmail.com>
2014-01-24 15:49:33 +0800

    android/MediaInfo: simplify

--- android/ijkmediaplayer/src/tv/danmaku/ijk/media/player/misc/MediaInfo.java
+++ android/ijkmediaplayer/src/tv/danmaku/ijk/media/player/misc/MediaInfo.java
@@ -1,46 +1,17 @@
 package tv.danmaku.ijk.media.player.misc;
 
-import android.os.Bundle;
 import android.text.TextUtils;
 
 public class MediaInfo {
-    private static final String BUNDLE_VIDEO_DECODER = "video_decoder";
-    private static final String BUNDLE_VIDEO_DECODER_IMPL = "video_decoder_impl";
-
-    private static final String BUNDLE_AUDIO_DECODER = "audio_decoder";
-    private static final String BUNDLE_AUDIO_DECODER_IMPL = "audio_decoder_impl";
-
     public String mVideoDecoder;
     public String mVideoDecoderImpl;
 
     public String mAudioDecoder;
     public String mAudioDecoderImpl;
 
-    public static MediaInfo sAndroidMediaInfo;
-    public static MediaInfo sAndroidListMediaInfo;
-
-    public static MediaInfo parseModuleInfo(Bundle args) {
-        MediaInfo moduleInfo = new MediaInfo();
-
-        moduleInfo.mVideoDecoder = args.getString(BUNDLE_VIDEO_DECODER);
-        moduleInfo.mVideoDecoderImpl = args
-                .getString(BUNDLE_VIDEO_DECODER_IMPL);
-
-        moduleInfo.mAudioDecoder = args.getString(BUNDLE_AUDIO_DECODER);
-        moduleInfo.mAudioDecoderImpl = args
-                .getString(BUNDLE_AUDIO_DECODER_IMPL);
-
-        return moduleInfo;
-    }
-
-    public static MediaInfo getNullModuleInfo() {
-        MediaInfo module = new MediaInfo();
-        return module;
-    }
-
-    public final String getVideoDecoderInline() {
+    public final String getVideoDecoderInline(String defaultValue) {
         if (TextUtils.isEmpty(mVideoDecoder))
-            return "N/A";
+            return defaultValue;
 
         if (mVideoDecoder.equalsIgnoreCase("mediacodec")) {
             mVideoDecoder = "MediaCodec";
@@ -58,9 +29,9 @@ public class MediaInfo {
         return sb.toString();
     }
 
-    public final String getAudioDecoderInline() {
+    public final String getAudioDecoderInline(String defaultValue) {
         if (TextUtils.isEmpty(mAudioDecoder))
-            return "N/A";
+            return defaultValue;
 
         if (mVideoDecoder.equalsIgnoreCase("mediacodec")) {
             mVideoDecoder = "MediaCodec";
