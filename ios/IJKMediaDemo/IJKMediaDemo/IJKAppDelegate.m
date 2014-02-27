02ea10fe86b636a1a693eaa96a94f0f4dbd7e2a2
bbcallen <bbcallen@gmail.com>
2014-02-27 19:57:02 +0800

    ios: simplify AppDelegate

--- ios/IJKMediaDemo/IJKMediaDemo/IJKAppDelegate.m
+++ ios/IJKMediaDemo/IJKMediaDemo/IJKAppDelegate.m
@@ -26,14 +26,12 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    [[IJKMediaModule sharedModule] applicationWillResignActive:application];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    [[IJKMediaModule sharedModule] applicationDidEnterBackground:application];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -44,13 +42,11 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [[IJKMediaModule sharedModule] applicationDidBecomeActive:application];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    [[IJKMediaModule sharedModule] applicationWillTerminate:application];
 }
 
 @end
