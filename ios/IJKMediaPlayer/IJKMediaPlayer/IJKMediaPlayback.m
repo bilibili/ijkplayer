02f6cb44245383dd8415ad31a876a5d6b2ec811c
Yuvesy <yuvesy@yuvesy.com>
2014-07-29 14:16:58 +0800

    硬解缓冲字节数

--- ios/IJKMediaPlayer/IJKMediaPlayer/IJKMediaPlayback.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/IJKMediaPlayback.m
@@ -27,3 +27,5 @@ NSString *const IJKMediaPlaybackIsPreparedToPlayDidChangeNotification = @"IJKMed
 NSString *const IJKMoviePlayerLoadStateDidChangeNotification = @"IJKMoviePlayerLoadStateDidChangeNotification";
 NSString *const IJKMoviePlayerPlaybackDidFinishNotification = @"IJKMoviePlayerPlaybackDidFinishNotification";
 NSString *const IJKMoviePlayerPlaybackStateDidChangeNotification = @"IJKMoviePlayerPlaybackStateDidChangeNotification";
+
+NSString *const IJKMoviePlayerNumberOfBytesTransferredNotification = @"IJKMoviePlayerNumberOfBytesTransferredNotification";
