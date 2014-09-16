14ede8a01b53058a6517034546a222e949e055e8
Zhang Rui <bbcallen@gmail.com>
2014-09-16 12:55:28 +0800

    build/ios: build openssl

--- init-ios-openssl.sh
+++ init-ios-openssl.sh
@@ -2,7 +2,7 @@
 
 IJK_OPENSSL_UPSTREAM=https://github.com/openssl/openssl
 IJK_OPENSSL_FORK=https://github.com/bbcallen/openssl.git
-IJK_OPENSSL_COMMIT=OpenSSL_1_0_1g
+IJK_OPENSSL_COMMIT=OpenSSL_1_0_1i
 IJK_OPENSSL_LOCAL_REPO=extra/openssl
 
 set -e
@@ -14,9 +14,9 @@ sh $TOOLS/pull-repo-base.sh $IJK_OPENSSL_UPSTREAM $IJK_OPENSSL_LOCAL_REPO
 function pull_fork()
 {
     echo "== pull openssl fork $1 =="
-    sh $TOOLS/pull-repo-ref.sh $IJK_FFMPEG_FORK ios/openssl-$1 ${IJK_FFMPEG_LOCAL_REPO}
+    sh $TOOLS/pull-repo-ref.sh $IJK_OPENSSL_FORK ios/openssl-$1 ${IJK_OPENSSL_LOCAL_REPO}
     cd ios/openssl-$1
-    git checkout ${IJK_FFMPEG_COMMIT}
+    git checkout ${IJK_OPENSSL_COMMIT}
     cd -
 }
 
