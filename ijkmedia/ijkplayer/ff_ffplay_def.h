ad7bb80c4285225b44b721a5401e97b24945fc9a
Zhang Rui <bbcallen@gmail.com>
2014-11-07 10:21:01 +0800

    ff_ffplay: unlimited MIN_FRAMES works

--- ijkmedia/ijkplayer/ff_ffplay_def.h
+++ ijkmedia/ijkplayer/ff_ffplay_def.h
@@ -43,7 +43,7 @@
 #define BUFFERING_CHECK_PER_MILLISECONDS        (500)
 
 #define MAX_QUEUE_SIZE (10 * 1024 * 1024)
-#define MIN_FRAMES 5
+#define MIN_FRAMES 50000
 
 /* Minimum SDL audio buffer size, in samples. */
 #define SDL_AUDIO_MIN_BUFFER_SIZE 512
