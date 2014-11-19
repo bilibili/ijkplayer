1f499b03203faeb8366e04f8f530a7f22a221a51
Zhang Rui <bbcallen@gmail.com>
2014-11-19 22:37:35 +0800

    ijkutil: no cpp any more

---
+++ ijkmedia/ijkutil/android/jnihelp.c
@@ -0,0 +1,56 @@
+/*
+ * jnihelp.c
+ *
+ * Copyright (c) 2014 Zhang Rui <bbcallen@gmail.com>
+ *
+ * This file is part of ijkPlayer.
+ *
+ * ijkPlayer is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * ijkPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ijkPlayer; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "jnihelp.h"
+#include "loghelp.h"
+
+int jniThrowException(JNIEnv* env, const char* className, const char* msg)
+{
+    if ((*env)->ExceptionCheck(env)) {
+        jthrowable exception = (*env)->ExceptionOccurred(env);
+        (*env)->ExceptionClear(env);
+
+        if (exception != NULL) {
+            ALOGW("Discarding pending exception (%s) to throw", className);
+            (*env)->DeleteLocalRef(env, exception);
+        }
+    }
+
+    jclass exceptionClass = (*env)->FindClass(env, className);
+    if (exceptionClass == NULL) {
+        ALOGE("Unable to find exception class %s", className);
+        /* ClassNotFoundException now pending */
+        goto fail;
+    }
+
+    if ((*env)->ThrowNew(env, exceptionClass, msg) != JNI_OK) {
+        ALOGE("Failed throwing '%s' '%s'", className, msg);
+        /* an exception, most likely OOM, will now be pending */
+        goto fail;
+    }
+
+    return 0;
+fail:
+    if (exceptionClass)
+        (*env)->DeleteLocalRef(env, exceptionClass);
+    return -1;
+}
