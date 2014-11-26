de758e2654a74d4d0f41c01617416a36e8cfb359
swordfeng <swordfeng@qq.com>
2014-11-26 20:02:09 +0800

    Android: add User Agent support to VideoView

--- android/ijkmediawidget/src/tv/danmaku/ijk/media/widget/VideoView.java
+++ android/ijkmediawidget/src/tv/danmaku/ijk/media/widget/VideoView.java
@@ -64,6 +64,7 @@ public class VideoView extends SurfaceView implements
 
     private Uri mUri;
     private long mDuration;
+    private String mUserAgent;
 
     private static final int STATE_ERROR = -1;
     private static final int STATE_IDLE = 0;
@@ -216,6 +217,10 @@ public class VideoView extends SurfaceView implements
         invalidate();
     }
 
+    public void setUserAgent(String ua) {
+    	mUserAgent = ua;
+    }
+    
     public void stopPlayback() {
         if (mMediaPlayer != null) {
             mMediaPlayer.stop();
@@ -244,6 +249,12 @@ public class VideoView extends SurfaceView implements
                 ijkMediaPlayer = new IjkMediaPlayer();
                 ijkMediaPlayer.setAvOption(AvFormatOption_HttpDetectRangeSupport.Disable);
                 ijkMediaPlayer.setOverlayFormat(AvFourCC.SDL_FCC_RV32);
+
+                ijkMediaPlayer.setAvCodecOption("skip_loop_filter", "48");
+                ijkMediaPlayer.setFrameDrop(12);
+                if (mUserAgent != null) {
+                    ijkMediaPlayer.setAvFormatOption("user_agent", mUserAgent);
+                							}
             }
             mMediaPlayer = ijkMediaPlayer;
             mMediaPlayer.setOnPreparedListener(mPreparedListener);
