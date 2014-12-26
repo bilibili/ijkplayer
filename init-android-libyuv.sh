318f9eceb4defbbbfb7f22edd2e74a716365eb50
Zhang Rui <bbcallen@gmail.com>
2014-12-26 16:00:23 +0800

    build/android: upgrade to latest libyuv

--- init-android-libyuv.sh
+++ init-android-libyuv.sh
@@ -18,7 +18,7 @@
 
 IJK_LIBYUV_UPSTREAM=https://github.com/bbcallen/libyuv.git
 IJK_LIBYUV_FORK=https://github.com/bbcallen/libyuv.git
-IJK_LIBYUV_COMMIT=master
+IJK_LIBYUV_COMMIT=ijk-r0.2.1-dev
 IJK_LIBYUV_LOCAL_REPO=extra/libyuv
 
 set -e
