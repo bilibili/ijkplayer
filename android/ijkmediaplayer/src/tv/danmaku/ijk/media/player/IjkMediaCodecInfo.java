e193eddf92244170275c3e91d473ede155e2f03e
Zhang Rui <bbcallen@gmail.com>
2015-01-05 11:41:52 +0800

    MediaCodec: prefer OMX.SEC.avc.dec than OMX.SEC.avcdec for reorder support

--- android/ijkmediaplayer/src/tv/danmaku/ijk/media/player/IjkMediaCodecInfo.java
+++ android/ijkmediaplayer/src/tv/danmaku/ijk/media/player/IjkMediaCodecInfo.java
@@ -60,6 +60,9 @@ public class IjkMediaCodecInfo {
         //      Exynos 3110
         //          Nexus S
         sKnownCodecList.put("OMX.SEC.AVC.Decoder", RANK_TESTED);
+        // OMX.SEC.avcdec doesn't reorder output pictures on GT-9100
+        sKnownCodecList.put("OMX.SEC.avc.dec", RANK_TESTED - 1);
+        sKnownCodecList.put("OMX.SEC.avcdec", RANK_TESTED - 2);
 
         // ----- TI -----
         //      TI OMAP4460
