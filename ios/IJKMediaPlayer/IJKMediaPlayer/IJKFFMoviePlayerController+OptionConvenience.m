2f6d3638b0a311534d79c68d3dc4bacfff5ac2d8
Zhang Rui <bbcallen@gmail.com>
2015-06-10 17:26:03 +0800

    ios: add license header

--- ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController+OptionConvenience.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController+OptionConvenience.m
@@ -1,10 +1,24 @@
-//
-//  IJKFFMoviePlayerController+OptionConvenience.m
-//  IJKMediaPlayer
-//
-//  Created by Zhang Rui on 15/6/4.
-//  Copyright (c) 2015年 bilibili. All rights reserved.
-//
+/*
+ * IJKFFMoviePlayerController+OptionConvenience.m
+ *
+ * Copyright (c) 2015 Zhang Rui <bbcallen@gmail.com>
+ *
+ * This file is part of ijkPlayer.
+ *
+ * ijkPlayer is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * ijkPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ijkPlayer; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 #import "IJKFFMoviePlayerController+OptionConvenience.h"
 
