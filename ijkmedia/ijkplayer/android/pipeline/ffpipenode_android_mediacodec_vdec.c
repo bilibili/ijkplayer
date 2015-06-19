b0e50d8efd3bf92fea004d65241c7319b23408d0
Zhang Rui <bbcallen@gmail.com>
2015-06-19 17:02:37 +0800

    android/MediaCodec: remove unused code

--- ijkmedia/ijkplayer/android/pipeline/ffpipenode_android_mediacodec_vdec.c
+++ ijkmedia/ijkplayer/android/pipeline/ffpipenode_android_mediacodec_vdec.c
@@ -968,7 +968,6 @@ fallback_to_ffplay:
 
 static int func_flush(IJKFF_Pipenode *node)
 {
-    JNIEnv                *env      = NULL;
     IJKFF_Pipenode_Opaque *opaque   = node->opaque;
 
     if (!opaque)
