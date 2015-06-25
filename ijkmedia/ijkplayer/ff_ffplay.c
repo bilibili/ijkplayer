edd01c92bbd6d080d7dafefe38c15da3ecd6ca43
Zhang Rui <bbcallen@gmail.com>
2015-06-25 13:01:37 +0800

    ff_ffplay: use av_stristart to detect rtmp

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -2258,7 +2258,7 @@ static int read_thread(void *arg)
         av_format_set_control_message_cb(ic, ffp_format_control_message);
         av_format_set_opaque(ic, ffp);
     }
-    if (av_strstart(is->filename, "rtmp", NULL)) {
+    if (av_stristart(is->filename, "rtmp", NULL)) {
         // There is total different meaning for 'timeout' option in rtmp
         av_log(NULL, AV_LOG_WARNING, "remove 'timeout' option for rtmp.\n");
         av_dict_set(&ffp->format_opts, "timeout", NULL, 0);
