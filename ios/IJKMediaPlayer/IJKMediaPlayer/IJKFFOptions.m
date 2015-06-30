6a49addedb21bd58dee8ccf1b600d99175f9175a
Zhang Rui <bbcallen@gmail.com>
2015-06-30 17:01:27 +0800

    ios/options: fix warning

--- ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFOptions.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFOptions.m
@@ -75,12 +75,12 @@
         [categoryDict enumerateKeysAndObjectsUsingBlock:^(id optKey, id optValue, BOOL *stop) {
             if ([optValue isKindOfClass:[NSNumber class]]) {
                 ijkmp_set_option_int(mediaPlayer,
-                                     [categoryKey integerValue],
+                                     (int)[categoryKey integerValue],
                                      [optKey UTF8String],
                                      [optValue longLongValue]);
             } else if ([optValue isKindOfClass:[NSString class]]) {
                 ijkmp_set_option(mediaPlayer,
-                                 [categoryKey integerValue],
+                                 (int)[categoryKey integerValue],
                                  [optKey UTF8String],
                                  [optValue UTF8String]);
             }
