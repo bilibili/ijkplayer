7ecdb958ce6690d088b3fdccbdb22608504cb924
Zhang Rui <bbcallen@gmail.com>
2015-07-01 16:10:01 +0800

    build/android: add clean command to compile-ijk.sh

--- android/compile-ijk.sh
+++ android/compile-ijk.sh
@@ -22,42 +22,64 @@ if [ -z "$ANDROID_NDK" -o -z "$ANDROID_NDK" ]; then
 fi
 
 REQUEST_TARGET=$1
-REQUEST_PROF=$2
+REQUEST_SUB_CMD=$2
 ALL_ABI="armv5 armv7a x86"
 
-do_config_prof () {
-    PROF_ENABLED=$1
+do_sub_cmd () {
+    SUB_CMD=$1
     if [ -L "./android-ndk-prof" ]; then
         rm android-ndk-prof
     fi
-    if [ "$1" = "prof" ]; then 
+
+    if [ "$PARAM_SUB_CMD" = 'prof' ]; then
+        echo 'profiler build: YES';
         ln -s ../../../../../../ijkprof/android-ndk-profiler/jni android-ndk-prof
+    else
+        echo 'profiler build: NO';
+        ln -s ../../../../../../ijkprof/android-ndk-profiler-dummy/jni android-ndk-prof
     fi
+
+    case $SUB_CMD in
+        prof)
+            $ANDROID_NDK/ndk-build
+        ;;
+        clean)
+            $ANDROID_NDK/ndk-build clean
+        ;;
+        rebuild)
+            $ANDROID_NDK/ndk-build clean
+            $ANDROID_NDK/ndk-build
+        ;;
+        *)
+            $ANDROID_NDK/ndk-build
+        ;;
+    esac
 }
 
 do_ndk_build () {
     PARAM_TARGET=$1
+    PARAM_SUB_CMD=$2
     case "$PARAM_TARGET" in
         armv7a)
             cd ijkplayer/player-armv7a/src/main/jni
-            do_config_prof $REQUEST_PROF
-            $ANDROID_NDK/ndk-build
+            do_sub_cmd $PARAM_SUB_CMD
             cd -
         ;;
         armv5)
             cd ijkplayer/player-armv5/src/main/jni
-            do_config_prof $REQUEST_PROF
-            $ANDROID_NDK/ndk-build
+            do_sub_cmd $PARAM_SUB_CMD
             cd -
         ;;
         x86)
             cd ijkplayer/player-x86/src/main/jni
-            $ANDROID_NDK/ndk-build
+            if [ "$PARAM_SUB_CMD" = 'prof' ]; then PARAM_SUB_CMD=''; fi
+            do_sub_cmd $PARAM_SUB_CMD
             cd -
         ;;
         arm64)
             cd ijkplayer/player-arm64/src/main/jni
-            $ANDROID_NDK/ndk-build
+            if [ "$PARAM_SUB_CMD" = 'prof' ]; then PARAM_SUB_CMD=''; fi
+            do_sub_cmd $PARAM_SUB_CMD
             cd -
         ;;
     esac
@@ -69,18 +91,25 @@ case "$REQUEST_TARGET" in
         do_ndk_build armv7a;
     ;;
     armv5|armv7a|x86|arm64)
-        do_ndk_build $REQUEST_TARGET;
+        do_ndk_build $REQUEST_TARGET $REQUEST_SUB_CMD;
     ;;
     all)
         for ABI in $ALL_ABI
         do
-            do_ndk_build "$ABI";
+            do_ndk_build "$ABI" $REQUEST_SUB_CMD;
+        done
+    ;;
+    clean)
+        for ABI in $ALL_ABI
+        do
+            do_ndk_build "$ABI" clean;
         done
     ;;
     *)
         echo "Usage:"
         echo "  compile-ijk.sh armv5|armv7a|x86|arm64"
         echo "  compile-ijk.sh all"
+        echo "  compile-ijk.sh clean"
     ;;
 esac
 
