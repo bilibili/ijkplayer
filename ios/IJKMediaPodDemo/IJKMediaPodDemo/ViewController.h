a12ff27d93d13c027e1d83c05068e412114169ea
Zhang Rui <bbcallen@gmail.com>
2015-07-23 18:45:02 +0800

    ios/IJKMediaPodDemo: pod example

---
+++ ios/IJKMediaPodDemo/IJKMediaPodDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IJKMediaPodDemo
+//
+//  Created by Zhang Rui on 15/7/23.
+//  Copyright (c) 2015年 Zhang Rui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
