5ef1effa4d2ab777f8a2dc4ddd6cccb5763c8ac7
施灵凯 <manshilingkai@gmail.com>
2015-08-03 14:01:13 +0800

    remove gson,add gson jar

--- android/ijkmediawidget/src/com/google/gson/internal/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Do NOT use any class in this package as they are meant for internal use in Gson.
- * These classes will very likely change incompatibly in future versions. You have been warned.
- *
- * @author Inderjeet Singh, Joel Leitch, Jesse Wilson
- */
-package com.google.gson.internal;
