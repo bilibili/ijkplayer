831614e4377bf51b12f07b881acfeeea4eaa9371
Zhang Rui <bbcallen@gmail.com>
2015-08-19 21:33:26 +0800

    android/TextureRenderView: should return true in onSurfaceTextureDestroyed

--- android/ijkplayer/sample/src/main/java/tv/danmaku/ijk/media/sample/widget/media/TextureRenderView.java
+++ android/ijkplayer/sample/src/main/java/tv/danmaku/ijk/media/sample/widget/media/TextureRenderView.java
@@ -228,7 +228,7 @@ public class TextureRenderView extends TextureView implements IRenderView{
                 renderCallback.onSurfaceDestroyed(surfaceHolder);
             }
 
-            return false;
+            return true;
         }
 
         @Override
