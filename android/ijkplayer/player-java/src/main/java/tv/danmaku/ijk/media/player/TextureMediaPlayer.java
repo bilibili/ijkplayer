1e3cfa421115c8bfe775446033bfd617c15be16e
Zhang Rui <bbcallen@gmail.com>
2015-09-06 15:08:15 +0800

    android/TextureMediaPlayer: getInternalMediaPlayer()

--- android/ijkplayer/player-java/src/main/java/tv/danmaku/ijk/media/player/TextureMediaPlayer.java
+++ android/ijkplayer/player-java/src/main/java/tv/danmaku/ijk/media/player/TextureMediaPlayer.java
@@ -37,6 +37,10 @@ public class TextureMediaPlayer extends MediaPlayerProxy implements IMediaPlayer
         }
     }
 
+    public IMediaPlayer getInternalMediaPlayer() {
+        return mBackEndMediaPlayer;
+    }
+
     //--------------------
     // IMediaPlayer
     //--------------------
