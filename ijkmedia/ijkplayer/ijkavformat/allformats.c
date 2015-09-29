fd1fcf6e9681e520491197788734db839a9874d8
Zhang Rui <bbcallen@gmail.com>
2015-09-29 11:22:37 +0800

    ijkavformat: always check protocol/demuxer name conflict

--- ijkmedia/ijkplayer/ijkavformat/allformats.c
+++ ijkmedia/ijkplayer/ijkavformat/allformats.c
@@ -25,35 +25,70 @@
 #include "libavformat/version.h"
 
 #define FF_REGISTER_PROTOCOL(x)                                         \
-{                                                                       \
-    extern URLProtocol ff_##x##_protocol;                               \
-    if (!ijkav_find_protocol(ff_##x##_protocol.name))                   \
-        ffurl_register_protocol(&ff_##x##_protocol);                    \
-}
+    {                                                                   \
+        extern URLProtocol ff_##x##_protocol;                           \
+        ijkav_register_protocol(&ff_##x##_protocol);                    \
+    }
 
 #define IJK_REGISTER_PROTOCOL(x)                                        \
     {                                                                   \
         extern URLProtocol ijkff_##x##_protocol;                        \
-        ffurl_register_protocol(&ijkff_##x##_protocol);                 \
+        ijkav_register_protocol(&ijkff_##x##_protocol);                 \
     }
 
 #define IJK_REGISTER_DEMUXER(x)                                         \
     {                                                                   \
         extern AVInputFormat ijkff_##x##_demuxer;                       \
-        av_register_input_format(&ijkff_##x##_demuxer);                 \
+        ijkav_register_input_format(&ijkff_##x##_demuxer);              \
     }
 
 
-static struct URLProtocol *ijkav_find_protocol(const char *proto_str)
+static struct URLProtocol *ijkav_find_protocol(const char *proto_name)
 {
     URLProtocol *up = NULL;
-
+    if (!proto_name)
+        return NULL;
     while ((up = ffurl_protocol_next(up)) != NULL) {
-        if (!strcmp(proto_str, up->name))
+        if (!up->name)
+            continue;
+        if (!strcmp(proto_name, up->name))
             break;
     }
-
     return up;
+}
+
+static struct AVInputFormat *ijkav_find_input_format(const char *iformat_name)
+{
+    AVInputFormat *fmt = NULL;
+    if (!iformat_name)
+        return NULL;
+    while ((fmt = av_iformat_next(fmt))) {
+        if (!fmt->name)
+            continue;
+        if (!strcmp(iformat_name, fmt->name))
+            return fmt;
+    }
+    return NULL;
+}
+
+static void ijkav_register_protocol(URLProtocol *protocol)
+{
+    if (ijkav_find_protocol(protocol->name)) {
+        av_log(NULL, AV_LOG_WARNING, "skip     protocol: %s (duplicated)\n", protocol->name);
+    } else {
+        av_log(NULL, AV_LOG_INFO,    "register protocol: %s\n", protocol->name);
+        ffurl_register_protocol(protocol);
+    }
+}
+
+static void ijkav_register_input_format(AVInputFormat *iformat)
+{
+    if (ijkav_find_input_format(iformat->name)) {
+        av_log(NULL, AV_LOG_WARNING, "skip     demuxer : %s (duplicated)\n", iformat->name);
+    } else {
+        av_log(NULL, AV_LOG_INFO,    "register demuxer : %s\n", iformat->name);
+        av_register_input_format(iformat);
+    }
 }
 
 void ijkav_register_all(void)
@@ -67,6 +102,7 @@ void ijkav_register_all(void)
     av_register_all();
 
     /* protocols */
+    av_log(NULL, AV_LOG_INFO, "===== custom modules begin =====\n");
     FF_REGISTER_PROTOCOL(async);
     IJK_REGISTER_PROTOCOL(ijkhttphook);
     IJK_REGISTER_PROTOCOL(ijkinject);
@@ -76,4 +112,5 @@ void ijkav_register_all(void)
 
     /* demuxers */
     IJK_REGISTER_DEMUXER(ijklivehook);
+    av_log(NULL, AV_LOG_INFO, "===== custom modules end =====\n");
 }
