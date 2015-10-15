e2f91211e788cdbb3e128a36a0716e91fdf6032f
Zhang Rui <bbcallen@gmail.com>
2015-10-15 15:57:02 +0800

    android/demo: request READ_EXTERNAL_STORAGE permission at runtime on Android 6.0 and above

--- android/ijkplayer/ijkplayer-sample/src/main/java/tv/danmaku/ijk/media/sample/application/AppActivity.java
+++ android/ijkplayer/ijkplayer-sample/src/main/java/tv/danmaku/ijk/media/sample/application/AppActivity.java
@@ -16,7 +16,11 @@
 
 package tv.danmaku.ijk.media.sample.application;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -28,6 +32,8 @@ import tv.danmaku.ijk.media.sample.activities.SampleMediaActivity;
 import tv.danmaku.ijk.media.sample.activities.SettingsActivity;
 
 public class AppActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +41,33 @@ public class AppActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // TODO: show explanation
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the task you need to do.
+                } else {
+                    // permission denied, boo! Disable the functionality that depends on this permission.
+                }
+                return;
+            }
+        }
     }
 
     @Override
