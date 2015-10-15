8687f39d472d4647f2ca6cc913452327e477f35d
Zhang Rui <bbcallen@gmail.com>
2015-10-15 11:49:09 +0800

    ffmpeg: update to ff2.8--ijk0.4.1.1--dev0.3.3--rc4

--- ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
@@ -32,7 +32,7 @@
 #include "ijkplayer/version.h"
 #include "ijkplayer/ijkavformat/ijkavformat.h"
 
-static const char *kIJKFFRequiredFFmpegVersion = "ff2.8--ijk0.4.1.1--dev0.3.3--rc2";
+static const char *kIJKFFRequiredFFmpegVersion = "ff2.8--ijk0.4.1.1--dev0.3.3--rc4";
 
 @interface IJKFFMoviePlayerController()
 
