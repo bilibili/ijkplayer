fc911192b01819937a0480bf38d78cc5a116ac30
Zhang Rui <bbcallen@gmail.com>
2016-04-20 18:49:04 +0800

    android/sample: fix lint: Redundant 'String.toString()'

--- android/ijkplayer/ijkplayer-sample/src/main/java/tv/danmaku/ijk/media/sample/content/PathCursor.java
+++ android/ijkplayer/ijkplayer-sample/src/main/java/tv/danmaku/ijk/media/sample/content/PathCursor.java
@@ -72,7 +72,7 @@ public class PathCursor extends AbstractCursor {
     public String getString(int column) {
         switch (column) {
             case CI_FILE_NAME:
-                return mFileList.get(getPosition()).file.getName().toString();
+                return mFileList.get(getPosition()).file.getName();
             case CI_FILE_PATH:
                 return mFileList.get(getPosition()).file.toString();
         }
