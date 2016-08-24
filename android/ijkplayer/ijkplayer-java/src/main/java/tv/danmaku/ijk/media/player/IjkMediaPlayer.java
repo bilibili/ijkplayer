7e6a10f2a7e6d3466e98fcd2a99beeb3870ee073
Xinzheng Zhang <zhangxzheng@gmail.com>
2016-08-24 17:06:52 +0800

    ijkMediaPlayer/cosmetic: indent property defination

--- android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
+++ android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
@@ -102,28 +102,28 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
 
     //----------------------------------------
     // properties
-    public static final int PROP_FLOAT_VIDEO_DECODE_FRAMES_PER_SECOND = 10001;
-    public static final int PROP_FLOAT_VIDEO_OUTPUT_FRAMES_PER_SECOND = 10002;
-    public static final int FFP_PROP_FLOAT_PLAYBACK_RATE              = 10003;
+    public static final int PROP_FLOAT_VIDEO_DECODE_FRAMES_PER_SECOND       = 10001;
+    public static final int PROP_FLOAT_VIDEO_OUTPUT_FRAMES_PER_SECOND       = 10002;
+    public static final int FFP_PROP_FLOAT_PLAYBACK_RATE                    = 10003;
 
-    public static final int FFP_PROP_INT64_SELECTED_VIDEO_STREAM      = 20001;
-    public static final int FFP_PROP_INT64_SELECTED_AUDIO_STREAM      = 20002;
+    public static final int FFP_PROP_INT64_SELECTED_VIDEO_STREAM            = 20001;
+    public static final int FFP_PROP_INT64_SELECTED_AUDIO_STREAM            = 20002;
 
-    public static final int FFP_PROP_INT64_VIDEO_DECODER              = 20003;
-    public static final int FFP_PROP_INT64_AUDIO_DECODER              = 20004;
-    public static final int     FFP_PROPV_DECODER_UNKNOWN             = 0;
-    public static final int     FFP_PROPV_DECODER_AVCODEC             = 1;
-    public static final int     FFP_PROPV_DECODER_MEDIACODEC          = 2;
-    public static final int     FFP_PROPV_DECODER_VIDEOTOOLBOX        = 3;
-    public static final int FFP_PROP_INT64_VIDEO_CACHED_DURATION      = 20005;
-    public static final int FFP_PROP_INT64_AUDIO_CACHED_DURATION      = 20006;
-    public static final int FFP_PROP_INT64_VIDEO_CACHED_BYTES         = 20007;
-    public static final int FFP_PROP_INT64_AUDIO_CACHED_BYTES         = 20008;
-    public static final int FFP_PROP_INT64_VIDEO_CACHED_PACKETS       = 20009;
-    public static final int FFP_PROP_INT64_AUDIO_CACHED_PACKETS       = 20010;
-    public static final int FFP_PROP_INT64_ASYNC_STATISTIC_BUF_BACKWARDS = 20201;
-    public static final int FFP_PROP_INT64_ASYNC_STATISTIC_BUF_FORWARDS  = 20202;
-    public static final int FFP_PROP_INT64_ASYNC_STATISTIC_BUF_CAPACITY  = 20203;
+    public static final int FFP_PROP_INT64_VIDEO_DECODER                    = 20003;
+    public static final int FFP_PROP_INT64_AUDIO_DECODER                    = 20004;
+    public static final int     FFP_PROPV_DECODER_UNKNOWN                   = 0;
+    public static final int     FFP_PROPV_DECODER_AVCODEC                   = 1;
+    public static final int     FFP_PROPV_DECODER_MEDIACODEC                = 2;
+    public static final int     FFP_PROPV_DECODER_VIDEOTOOLBOX              = 3;
+    public static final int FFP_PROP_INT64_VIDEO_CACHED_DURATION            = 20005;
+    public static final int FFP_PROP_INT64_AUDIO_CACHED_DURATION            = 20006;
+    public static final int FFP_PROP_INT64_VIDEO_CACHED_BYTES               = 20007;
+    public static final int FFP_PROP_INT64_AUDIO_CACHED_BYTES               = 20008;
+    public static final int FFP_PROP_INT64_VIDEO_CACHED_PACKETS             = 20009;
+    public static final int FFP_PROP_INT64_AUDIO_CACHED_PACKETS             = 20010;
+    public static final int FFP_PROP_INT64_ASYNC_STATISTIC_BUF_BACKWARDS    = 20201;
+    public static final int FFP_PROP_INT64_ASYNC_STATISTIC_BUF_FORWARDS     = 20202;
+    public static final int FFP_PROP_INT64_ASYNC_STATISTIC_BUF_CAPACITY     = 20203;
     //----------------------------------------
 
     @AccessedByNative
