848383b8dbbbf6e6d6b897960cf06f8cad5039b4
devslava <sviatoslav.kostenko@gmail.com>
2016-09-27 13:59:57 +0300

    Update ExoPlayer to 1.5.11
    with some reformat to match original format. -- by bbcallen
    
    Signed-off-by: Zhang Rui <bbcallen@gmail.com>

--- android/ijkplayer/ijkplayer-exo/src/main/java/tv/danmaku/ijk/media/exo/demo/player/DemoPlayer.java
+++ android/ijkplayer/ijkplayer-exo/src/main/java/tv/danmaku/ijk/media/exo/demo/player/DemoPlayer.java
@@ -15,6 +15,10 @@
  */
 package tv.danmaku.ijk.media.exo.demo.player;
 
+import android.media.MediaCodec.CryptoException;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.Surface;
 import com.google.android.exoplayer.CodecCounters;
 import com.google.android.exoplayer.DummyTrackRenderer;
 import com.google.android.exoplayer.ExoPlaybackException;
@@ -42,12 +46,6 @@ import com.google.android.exoplayer.upstream.BandwidthMeter;
 import com.google.android.exoplayer.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer.util.DebugTextViewHelper;
 import com.google.android.exoplayer.util.PlayerControl;
-
-import android.media.MediaCodec.CryptoException;
-import android.os.Handler;
-import android.os.Looper;
-import android.view.Surface;
-
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
