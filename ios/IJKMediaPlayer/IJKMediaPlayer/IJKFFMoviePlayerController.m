1aea6ad3f51052054d32848be8e31ed3c8cf173b
Xinzheng Zhang <zhangxzheng@gmail.com>
2016-11-04 19:19:50 +0800

    IJKFFMoviePlayer: fix compile error

--- ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
@@ -32,7 +32,7 @@
 
 #include "string.h"
 
-static const char *kIJKFFRequiredFFmpegVersion = "ff3.2--ijk0.7.0--20161102--001"
+static const char *kIJKFFRequiredFFmpegVersion = "ff3.2--ijk0.7.0--20161102--001";
 
 // It means you didn't call shutdown if you found this object leaked.
 @interface IJKWeakHolder : NSObject
