c099cd64e441184c74a486cfb9dad5c937a8cf1b
Xinzheng Zhang <zhangxzheng@gmail.com>
2016-11-09 19:30:57 +0800

    ff_ffplay: fix sample buffer leak introduced in de0927d64

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -1249,7 +1249,9 @@ static int queue_picture(FFPlayer *ffp, AVFrame *src_frame, double pts, double d
         vp->bmp->sar_num = vp->sar.num;
         vp->bmp->sar_den = vp->sar.den;
 
+#ifdef FFP_MERGE
         av_frame_move_ref(vp->frame, src_frame);
+#endif
         frame_queue_push(&is->pictq);
         if (!is->viddec.first_frame_decoded) {
             ALOGD("Video: first frame decoded\n");
