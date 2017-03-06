a871a5be63fd094674b53fd73871ebfe2b8496ce
zhangzifeng02 <zhangzifeng02@meituan.com>
2017-03-06 17:30:18 +0800

    fix some static-analyze bugs

--- ios/IJKMediaPlayer/IJKMediaPlayer/IJKMediaPlayback.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/IJKMediaPlayback.m
@@ -98,7 +98,7 @@ NSString *const IJKMPMoviePlayerDidSeekCompleteErrorKey = @"IJKMPMoviePlayerDidS
     if (url == _url)
         return;
 
-    if ([self.url compare:url]) {
+    if (![self.url isEqualToString:url]) {
         _urlChanged = YES;
         _url = url;
     }
