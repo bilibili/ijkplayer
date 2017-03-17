c504b6c753604ab25f78634ec486be13d8aedaae
raymondzheng <raymondzheng1412@gmail.com>
2017-03-17 18:35:29 +0800

    ijkplayer-j4a: rename ijkio androidio

--- ijkmedia/ijkj4a/java/tv/danmaku/ijk/media/player/misc/IAndroidIO.java
+++ ijkmedia/ijkj4a/java/tv/danmaku/ijk/media/player/misc/IAndroidIO.java
@@ -1,7 +1,7 @@
 package tv.danmaku.ijk.media.player.misc;
 
 @SimpleCClassName
-public interface IIjkIOHttp {
+public interface IAndroidIO {
     int  open();
     int  read(byte[] buffer, int size);
     long seek(long offset, int whence);
