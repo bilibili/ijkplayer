9eaaa0e0db55c9c093f22924841014e62f9bba65
raymondzheng <raymondzheng1412@gmail.com>
2017-03-21 14:04:29 +0800

    ijkplayer-java: modify androidio open

--- android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/misc/IAndroidIO.java
+++ android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/misc/IAndroidIO.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 
 @SuppressWarnings("RedundantThrows")
 public interface IAndroidIO {
-    int  open() throws IOException;
+    int  open(String url) throws IOException;
     int  read(byte[] buffer, int size) throws IOException;
     long seek(long offset, int whence) throws IOException;
     int  close() throws IOException;
