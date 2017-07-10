cd2f0885600a51eb95cc3f752176d6b53269ce9d
raymondzheng <raymondzheng1412@gmail.com>
2017-07-10 18:54:26 +0800

    ffmpeg: update to ff3.3--ijk0.8.0--20170710--001

--- ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
@@ -33,7 +33,7 @@
 #import "ijkioapplication.h"
 #include "string.h"
 
-static const char *kIJKFFRequiredFFmpegVersion = "ff3.3--ijk0.8.0--20170626--001";
+static const char *kIJKFFRequiredFFmpegVersion = "ff3.3--ijk0.8.0--20170710--001";
 
 // It means you didn't call shutdown if you found this object leaked.
 @interface IJKWeakHolder : NSObject
