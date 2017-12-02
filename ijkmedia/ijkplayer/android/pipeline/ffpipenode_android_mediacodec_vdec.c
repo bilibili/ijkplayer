fd54c3016fe69f251699df0994fa3ab42316bf1d
yuazhen <zhengyuan10503@gmail.com>
2017-12-02 21:54:44 +0800

    skip restore esds for divx

--- ijkmedia/ijkplayer/android/pipeline/ffpipenode_android_mediacodec_vdec.c
+++ ijkmedia/ijkplayer/android/pipeline/ffpipenode_android_mediacodec_vdec.c
@@ -237,13 +237,18 @@ static int recreate_format_l(JNIEnv *env, IJKFF_Pipenode *node)
             free(convert_buffer);
 #endif
         } else if (opaque->codecpar->codec_id == AV_CODEC_ID_MPEG4) {
-            size_t esds_dec_dscr_type_length = opaque->codecpar->extradata_size + 0x18;
-            size_t esds_es_dscr_type_length = esds_dec_dscr_type_length + 0x08;
-            size_t esds_size = esds_es_dscr_type_length + 0x05;
-            uint8_t *convert_buffer = (uint8_t *)calloc(1, esds_size);
-            restore_mpeg4_esds(opaque->codecpar, opaque->codecpar->extradata, opaque->codecpar->extradata_size, esds_es_dscr_type_length, esds_dec_dscr_type_length, convert_buffer);
-            SDL_AMediaFormat_setBuffer(opaque->input_aformat, "csd-0", convert_buffer, esds_size);
-            free(convert_buffer);
+            if ((opaque->codecpar->codec_tag & 0x0000FFFF) == 0x00005844) {
+                ALOGD("skip restore esds for divx(codec_tag is DX40 or DX50 etc.)\n");
+                SDL_AMediaFormat_setBuffer(opaque->input_aformat, "csd-0", opaque->codecpar->extradata, opaque->codecpar->extradata_size);
+            } else {
+                size_t esds_dec_dscr_type_length = opaque->codecpar->extradata_size + 0x18;
+                size_t esds_es_dscr_type_length = esds_dec_dscr_type_length + 0x08;
+                size_t esds_size = esds_es_dscr_type_length + 0x05;
+                uint8_t *convert_buffer = (uint8_t *)calloc(1, esds_size);
+                restore_mpeg4_esds(opaque->codecpar, opaque->codecpar->extradata, opaque->codecpar->extradata_size, esds_es_dscr_type_length, esds_dec_dscr_type_length, convert_buffer);
+                SDL_AMediaFormat_setBuffer(opaque->input_aformat, "csd-0", convert_buffer, esds_size);
+                free(convert_buffer);
+            }
         } else {
             // Codec specific data
             // SDL_AMediaFormat_setBuffer(opaque->aformat, "csd-0", opaque->codecpar->extradata, opaque->codecpar->extradata_size);
@@ -1739,10 +1744,6 @@ IJKFF_Pipenode *ffpipenode_create_video_decoder_from_android_mediacodec(FFPlayer
     case AV_CODEC_ID_MPEG4:
         if (!ffp->mediacodec_mpeg4 && !ffp->mediacodec_all_videos) {
             ALOGE("%s: MediaCodec/MPEG4 is disabled. codec_id:%d \n", __func__, opaque->codecpar->codec_id);
-            goto fail;
-        }
-        if ((opaque->codecpar->codec_tag & 0x0000FFFF) == 0x00005844) {
-            ALOGE("%s: divx is not supported \n", __func__);
             goto fail;
         }
         strcpy(opaque->mcc.mime_type, SDL_AMIME_VIDEO_MPEG4);
