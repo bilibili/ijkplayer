e70894d88e2d1dcde31dff670cb803d64f497a13
luern0313 <liupeiran0313@163.com>
2019-12-25 14:21:47 +0800

    修复设置header时:后没有空格导致默认header会替换设置的header的问题

--- android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
+++ android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
@@ -422,7 +422,7 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
             StringBuilder sb = new StringBuilder();
             for(Map.Entry<String, String> entry: headers.entrySet()) {
                 sb.append(entry.getKey());
-                sb.append(":");
+                sb.append(": ");
                 String value = entry.getValue();
                 if (!TextUtils.isEmpty(value))
                     sb.append(entry.getValue());
