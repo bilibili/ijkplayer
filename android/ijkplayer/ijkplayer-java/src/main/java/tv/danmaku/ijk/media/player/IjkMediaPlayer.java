fb1e2781a273327c2b32b741d100279335f7bf1a
befovy <befovy@gmail.com>
2020-05-10 17:08:04 +0800

    feat: add loglevel for java code

--- android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
+++ android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
@@ -438,7 +438,7 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
             }
         }
 
-        Log.d(TAG, "Couldn't open file on client side, trying server side");
+        DebugLog.d(TAG, "Couldn't open file on client side, trying server side");
 
         setDataSource(uri.toString(), headers);
     }
@@ -1371,12 +1371,12 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
             if (TextUtils.isEmpty(mimeType))
                 return null;
 
-            Log.i(TAG, String.format(Locale.US, "onSelectCodec: mime=%s, profile=%d, level=%d", mimeType, profile, level));
+            DebugLog.i(TAG, String.format(Locale.US, "onSelectCodec: mime=%s, profile=%d, level=%d", mimeType, profile, level));
             ArrayList<IjkMediaCodecInfo> candidateCodecList = new ArrayList<IjkMediaCodecInfo>();
             int numCodecs = MediaCodecList.getCodecCount();
             for (int i = 0; i < numCodecs; i++) {
                 MediaCodecInfo codecInfo = MediaCodecList.getCodecInfoAt(i);
-                Log.d(TAG, String.format(Locale.US, "  found codec: %s", codecInfo.getName()));
+                DebugLog.d(TAG, String.format(Locale.US, "  found codec: %s", codecInfo.getName()));
                 if (codecInfo.isEncoder())
                     continue;
 
@@ -1388,7 +1388,7 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
                     if (TextUtils.isEmpty(type))
                         continue;
 
-                    Log.d(TAG, String.format(Locale.US, "    mime: %s", type));
+                    DebugLog.d(TAG, String.format(Locale.US, "    mime: %s", type));
                     if (!type.equalsIgnoreCase(mimeType))
                         continue;
 
@@ -1397,7 +1397,7 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
                         continue;
 
                     candidateCodecList.add(candidate);
-                    Log.i(TAG, String.format(Locale.US, "candidate codec: %s rank=%d", codecInfo.getName(), candidate.mRank));
+                    DebugLog.i(TAG, String.format(Locale.US, "candidate codec: %s rank=%d", codecInfo.getName(), candidate.mRank));
                     candidate.dumpProfileLevels(mimeType);
                 }
             }
@@ -1415,16 +1415,22 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
             }
 
             if (bestCodec.mRank < IjkMediaCodecInfo.RANK_LAST_CHANCE) {
-                Log.w(TAG, String.format(Locale.US, "unaccetable codec: %s", bestCodec.mCodecInfo.getName()));
+                DebugLog.w(TAG, String.format(Locale.US, "unaccetable codec: %s", bestCodec.mCodecInfo.getName()));
                 return null;
             }
 
-            Log.i(TAG, String.format(Locale.US, "selected codec: %s rank=%d", bestCodec.mCodecInfo.getName(), bestCodec.mRank));
+            DebugLog.i(TAG, String.format(Locale.US, "selected codec: %s rank=%d", bestCodec.mCodecInfo.getName(), bestCodec.mRank));
             return bestCodec.mCodecInfo.getName();
         }
     }
 
     public static native void native_profileBegin(String libName);
     public static native void native_profileEnd();
-    public static native void native_setLogLevel(int level);
+
+    public static void setLogLevel(int level) {
+        DebugLog.setLogLevel(level);
+        native_setLogLevel(level);
+    }
+
+    private static native void native_setLogLevel(int level);
 }
--- android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/pragma/DebugLog.java
+++ android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/pragma/DebugLog.java
@@ -17,122 +17,121 @@
 
 package tv.danmaku.ijk.media.player.pragma;
 
-import java.util.Locale;
-
-
 import android.util.Log;
 
-@SuppressWarnings({"SameParameterValue", "WeakerAccess"})
+import java.util.Locale;
+
 public class DebugLog {
-    public static final boolean ENABLE_ERROR = Pragma.ENABLE_VERBOSE;
-    public static final boolean ENABLE_INFO = Pragma.ENABLE_VERBOSE;
-    public static final boolean ENABLE_WARN = Pragma.ENABLE_VERBOSE;
-    public static final boolean ENABLE_DEBUG = Pragma.ENABLE_VERBOSE;
-    public static final boolean ENABLE_VERBOSE = Pragma.ENABLE_VERBOSE;
+
+    private static int level = Log.INFO;
+
+    public static void setLogLevel(int logLevel) {
+        level = logLevel;
+    }
 
     public static void e(String tag, String msg) {
-        if (ENABLE_ERROR) {
+        if (level <= Log.ERROR) {
             Log.e(tag, msg);
         }
     }
 
     public static void e(String tag, String msg, Throwable tr) {
-        if (ENABLE_ERROR) {
+        if (level <= Log.ERROR) {
             Log.e(tag, msg, tr);
         }
     }
 
     public static void efmt(String tag, String fmt, Object... args) {
-        if (ENABLE_ERROR) {
+        if (level <= Log.ERROR) {
             String msg = String.format(Locale.US, fmt, args);
             Log.e(tag, msg);
         }
     }
 
     public static void i(String tag, String msg) {
-        if (ENABLE_INFO) {
+        if (level <= Log.INFO) {
             Log.i(tag, msg);
         }
     }
 
     public static void i(String tag, String msg, Throwable tr) {
-        if (ENABLE_INFO) {
+        if (level <= Log.INFO) {
             Log.i(tag, msg, tr);
         }
     }
 
     public static void ifmt(String tag, String fmt, Object... args) {
-        if (ENABLE_INFO) {
+        if (level <= Log.INFO) {
             String msg = String.format(Locale.US, fmt, args);
             Log.i(tag, msg);
         }
     }
 
     public static void w(String tag, String msg) {
-        if (ENABLE_WARN) {
+        if (level <= Log.WARN) {
             Log.w(tag, msg);
         }
     }
 
     public static void w(String tag, String msg, Throwable tr) {
-        if (ENABLE_WARN) {
+        if (level <= Log.WARN) {
             Log.w(tag, msg, tr);
         }
     }
 
     public static void wfmt(String tag, String fmt, Object... args) {
-        if (ENABLE_WARN) {
+        if (level <= Log.WARN) {
             String msg = String.format(Locale.US, fmt, args);
             Log.w(tag, msg);
         }
     }
 
     public static void d(String tag, String msg) {
-        if (ENABLE_DEBUG) {
+        if (level <= Log.DEBUG) {
             Log.d(tag, msg);
         }
     }
 
     public static void d(String tag, String msg, Throwable tr) {
-        if (ENABLE_DEBUG) {
+        if (level <= Log.DEBUG) {
             Log.d(tag, msg, tr);
         }
     }
 
     public static void dfmt(String tag, String fmt, Object... args) {
-        if (ENABLE_DEBUG) {
+        if (level <= Log.DEBUG) {
             String msg = String.format(Locale.US, fmt, args);
             Log.d(tag, msg);
         }
     }
 
     public static void v(String tag, String msg) {
-        if (ENABLE_VERBOSE) {
+        if (level <= Log.VERBOSE) {
             Log.v(tag, msg);
         }
     }
 
     public static void v(String tag, String msg, Throwable tr) {
-        if (ENABLE_VERBOSE) {
+        if (level <= Log.VERBOSE) {
             Log.v(tag, msg, tr);
         }
     }
 
     public static void vfmt(String tag, String fmt, Object... args) {
-        if (ENABLE_VERBOSE) {
+        if (level <= Log.VERBOSE) {
             String msg = String.format(Locale.US, fmt, args);
             Log.v(tag, msg);
         }
     }
 
     public static void printStackTrace(Throwable e) {
-        if (ENABLE_WARN) {
+        if (level <= Log.ERROR) {
             e.printStackTrace();
         }
     }
 
     public static void printCause(Throwable e) {
-        if (ENABLE_WARN) {
+        if (level <= Log.ERROR) {
             Throwable cause = e.getCause();
             if (cause != null)
                 e = cause;
