e9932e09213bb3f19bc08d26a1d2b566ecce04f4
mihail <priamm@mail.ru>
2020-10-19 15:27:33 +0700

    Update NDK options

--- android/ijkplayer/ijkplayer-armv7a/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-armv7a/src/main/jni/Application.mk
@@ -18,12 +18,11 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 APP_OPTIM := release
-APP_PLATFORM := android-9
+APP_PLATFORM := android-16
 APP_ABI := armeabi-v7a
-NDK_TOOLCHAIN_VERSION=4.9
 APP_PIE := false
 
-APP_STL := stlport_static
+APP_STL := c++_static
 
 APP_CFLAGS := -O3 -Wall -pipe \
     -ffast-math \
