daa14de8588d686441dadc4c75736681b9f01203
mihail <priamm@mail.ru>
2020-10-19 15:31:26 +0700

    Update NDK options

--- android/ijkplayer/ijkplayer-x86/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-x86/src/main/jni/Application.mk
@@ -18,15 +18,14 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 APP_OPTIM := release
-APP_PLATFORM := android-9
+APP_PLATFORM := android-16
 APP_ABI := x86
-NDK_TOOLCHAIN_VERSION=4.9
 APP_PIE := false
 
-APP_STL := stlport_static
+APP_STL := c++_static
 
 APP_CFLAGS := -O3 -Wall -pipe \
     -ffast-math \
     -fstrict-aliasing -Werror=strict-aliasing \
-    -Wno-psabi -Wa,--noexecstack \
+    -Wa,--noexecstack \
     -DANDROID -DNDEBUG
--- android/ijkplayer/ijkplayer-x86_64/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-x86_64/src/main/jni/Application.mk
@@ -20,13 +20,12 @@
 APP_OPTIM := release
 APP_PLATFORM := android-21
 APP_ABI := x86_64
-NDK_TOOLCHAIN_VERSION=4.9
 APP_PIE := false
 
-APP_STL := stlport_static
+APP_STL := c++_static
 
 APP_CFLAGS := -O3 -Wall -pipe \
     -ffast-math \
     -fstrict-aliasing -Werror=strict-aliasing \
-    -Wno-psabi -Wa,--noexecstack \
+    -Wa,--noexecstack \
     -DANDROID -DNDEBUG
