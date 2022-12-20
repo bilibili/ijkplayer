a4437bf172101b97aaf95402b892d9d7a57047e9
Sam Jiang <jjksam@gmail.com>
2022-12-20 11:53:36 +0800

    use specific simulator compile

--- ios/IJKMediaPlayer/build.sh
+++ ios/IJKMediaPlayer/build.sh
@@ -1,4 +1,4 @@
 rm -rf build
-xcrun xcodebuild -scheme IJKMediaFrameworkWithSSL -target IJKMediaFrameworkWithSSL -configuration Debug -sdk iphonesimulator -destination "platform=iOS Simulator,name=Any iOS Simulator Device" build -derivedDataPath build && \
+xcrun xcodebuild -scheme IJKMediaFrameworkWithSSL -target IJKMediaFrameworkWithSSL -configuration Debug -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 5s" build -derivedDataPath build && \
 xcrun xcodebuild -scheme IJKMediaFrameworkWithSSL -target IJKMediaFrameworkWithSSL -configuration Release -sdk iphoneos build -derivedDataPath build && \
 xcrun xcodebuild -create-xcframework -framework build/Build/Products/Debug-iphonesimulator/IJKMediaFrameworkWithSSL.framework -framework build/Build/Products/Release-iphoneos/IJKMediaFrameworkWithSSL.framework -output build/Build/Products/IJKMediaFrameworkWithSSL.xcframework
